f801bda2ee2c322701976bda5d993994668ec724
mehelborn <mr.4uldum@gmail.com>
2025-11-04 09:54:53 +0700

    refactor(build): add .lock file and remove build script

--- build.py
+++
@@ -1,9 +0,0 @@
-import os
-import subprocess
-import shutil
-from pathlib import Path
-from hatchling.builders.hooks.plugin.interface import BuildHookInterface
-
-
-class CustomBuildHook(BuildHookInterface):
-    pass
